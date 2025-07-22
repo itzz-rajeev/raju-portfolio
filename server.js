311de30e088c86df21c87d3f4905ccbe5fd8e0b0
itzz-rajeev <rajeev@gmail.com>
2025-07-22 22:20:35 +0530

    Initial commit: portfolio

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static('public')); // Serves /public content
+// Serve static files from /public
+app.use(express.static('public'));
 
-// Serve pages
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+// Serve HTML pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+// Export app (for Vercel)
+module.exports = app;
